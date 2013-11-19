ada42924ac1a6a1cad53cb4a6d6781630f572172
Ankita Kanitkar <Ankita_Kanitkar@external.mckinsey.com>
2013-11-19 11:32:03 +0530

    comments

--- app/helpers/post_masters_helper.rb
+++ app/helpers/post_masters_helper.rb
@@ -1,2 +1,11 @@
 module PostMastersHelper
+
+	def bg_select(post_count)
+		 if post_count == 3 
+         style = "background-color: #81BEF7"
+        else
+          style = "background-color: white"  
+        end 
+	end
+
 end
